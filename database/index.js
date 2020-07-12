240637cf9ae61dd4d855a07e6ebe402161758b4a
Ian King <ian.antking@gmail.com>
2020-07-12 12:42:09 +0100

    refactor to less meta design pattern

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
-module.exports = {
-  development: {
+const ENV = process.env.NODE_ENV || 'development'
+
+const connection = {
     client: 'mysql2',
     connection: {
       host : '127.0.0.1',
@@ -7,5 +8,7 @@ module.exports = {
       password : 'password',
       database : 'music_library'
     }
-  },
-};
+  }
+const database = require('knex')(connection)
+
+module.exports = database
--- src/app.js
+++ src/app.js
@@ -1,21 +1,58 @@
 const express = require('express');
-const queries = require('../queries');
+const database = require('../database');
+const { where } = require('../database');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/artists', (req, res) => {
-    queries.insert('Artists', req.body).then(results => res.send(results));
+    database('Artists')
+        .insert(req.body)
+        .then((id) => {
+            database('Artists')
+                .where({id})
+                .then((user) => res.status(201).send(user));
+        })
+        .catch(error => res.status(500).send(error));
 })
 
 app.get('/artists', (req, res) => {
-    queries.get('Artists', req.query).then(results => {res.send(results)});
+    database('Artists')
+        .where(req.query)
+        .then((artists) => res.status(200).send(artists))
+        .catch(error => res.status(500).send(error));
 })
 
 app.get('/artists/:id', (req, res) => {
-    const id = req.params.id
-    queries.get('Artists', { id }).then(results => {res.send(results)});
+    const {id} = req.params;
+    database('Artists')
+        .where({id})
+        .then((artists) => res.status(200).send(artists))
+        .catch(error => res.status(500).send(error));
+})
+
+app.patch('/artists/:id', (req, res) => {
+    const {id} = req.params
+    database('Artists')
+        .where({id})
+        .update(req.body)
+        .then(() => {
+            database('Artists')
+                .where({id})
+                .then(newArtist => res.status(200).send(newArtist))
+                .catch(error => res.status(500).send(error));
+        })
+        .catch(error => res.status(500).send(error));
+})
+
+app.delete('/artists/:id', (req, res) => {
+    const {id} = req.params
+    database('Artists')
+        .where({id})
+        .del()
+        .then(() => res.sendStatus(200))
+        .catch(error => res.status(500).send(error));
 })
 
 module.exports = app;
